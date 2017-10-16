80db1c092c0ab8678761f079dcf79cd53040dd3b
Omar Trejo <otrenav@gmail.com>
2017-10-16 17:40:20 -0500

    Flask API

---
+++ api.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+from databases import MongoDatabase
+from users import User
+
+app = Flask(__name__)
+CORS(app)
+
+DB = MongoDatabase()
+
+#
+# TODO: Validate user is authenticated (careful with CORS, and CSRF)
+#
+
+
+@app.route('/assets/time-series', methods=['GET'])
+def assets_time_series():
+    user_id = request.args['user_id']
+    resp = {'success': True, 'message': ''}
+    resp['data'] = User(db=DB, user_id=user_id).assets_time_series()
+    return jsonify(resp)
+
+
+@app.route('/assets/current-status', methods=['GET'])
+def assets_current_status():
+    user_id = request.args['user_id']
+    resp = {'success': True, 'message': ''}
+    resp['data'] = User(db=DB, user_id=user_id).assets_current_status()
+    return jsonify(resp)
+
+
+@app.route('/assets/balances', methods=['GET'])
+def assets_balances():
+    user_id = request.args['user_id']
+    resp = {'success': True, 'message': ''}
+    resp['data'] = User(db=DB, user_id=user_id).assets_balances()
+    return jsonify(resp)
