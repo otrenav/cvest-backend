6fe0a1c54bab71613235719c16d378951cc7a808
Omar Trejo <otrenav@gmail.com>
2017-10-16 17:39:40 -0500

    Improved User class

--- users/user.py
+++ users/user.py
@@ -1,27 +1,69 @@
 
-from databases import MongoDatabase
+from utilities.exceptions import CVESTExternalError
+
+from assets.wallets import Wallet
+from assets.accounts import Account
 
 
 class User:
 
-    def __init__(self, email=''):
-        self.email = email
-        self.name = ''
-        self.assets = []
-        self.wallets = []
-        self.accounts = []
-        self.preferences = {}
-        self.db = MongoDatabase()
-        self._read_user_data()
+    name = ''
 
-    def update_assets(self):
-        pass
+    def __init__(self, email=None, db=None, new=False, user_id=None):
+        self.db = db
+        if user_id:
+            self._read_user("_id", user_id)
+        elif not new:
+            self._read_user("email", email)
+        else:
+            self._new_user(email)
 
-    def _read_user_data(self):
-        user = self.db.read_user_data(self.email)
+    def update_name(self, name):
+        self.db.update_user_name(self.email, name)
+        self.name = name
+        return self
+
+    def update_assets(self, timestamp):
+        assets = []
+        for wallet in self._wallets():
+            try:
+                assets += wallet.update_assets(timestamp, self.db)
+            except CVESTExternalError:
+                pass
+        for account in self._accounts():
+            try:
+                assets += account.update_assets(timestamp, self.db)
+            except CVESTExternalError:
+                pass
+        return assets
+
+    def assets_time_series(self):
+        return self.db.read_user_assets_time_series(self.user_id)
+
+    def assets_current_status(self):
+        return self.db.read_user_assets_current_status(self.user_id)
+
+    def assets_balances(self):
+        return self.db.read_user_assets_balances(self.user_id)
+
+    def new_wallet(self, symbol, address, note=""):
+        self.db.write_wallet(self.user_id, symbol, address, note)
+
+    def new_account(self, exchange, key, secret):
+        self.db.write_account(self.user_id, exchange, key, secret)
+
+    def _read_user(self, key, value):
+        user = self.db.read_user(key, value)
+        self.name = user.get("name")
         self.email = user["email"]
-        self.name = user["name"]
-        self.assets = user["assets"]
-        self.wallets = user["wallets"]
-        self.accounts = user["accounts"]
-        self.preferences = user["preferences"]
+        self.user_id = user["_id"]
+
+    def _new_user(self, email):
+        self.db.write_user(email)
+        self._read_user("email", email)
+
+    def _wallets(self):
+        return self.db.read_user_wallets(self.email, Wallet)
+
+    def _accounts(self):
+        return self.db.read_user_accounts(self.email, Account)
